5856894929291473e2775ddc3403a22b1930d50c
seijihosokawa <seijihosokawa@gmail.com>
2021-05-23 09:41:21 -0400

    create component for index boxes

--- App.js
+++ App.js
@@ -1,22 +1,22 @@
 const App = Vue.createApp({
     data(){
-        let indexList = ["%5EGSPC","%5EDJI","%5EIXI"];
-        var indexMap = new Map();
-        for(stock in indexList){
-            //getMarketPrice(stock);
-            //let marketPrice = getMarketPrice(indexList[stock]);
-            //indexMap.set(indexList[stock], marketPrice);
-        }
-        console.log(indexMap);
+        
         return {
-            name: 'S&P 500'
+            name: ''
         }
     }
 })
 
 App.component('market-index-box', {
     props: ['name'],
-    template: '<div class="bg-black border-red-600 dark:bg-gray-800 bg-opacity-95 border-opacity-60 | p-1 border-solid rounded-3xl border-2 | flex justify-around cursor-pointer | hover:bg-red-400 dark:hover:bg-red-600 hover:border-transparent | transition-colors duration-500"><div class="flex flex-col justify-center"><p class="text-base">{{ name }}</p><p class="text-xs">4,155.86</p><p class="text-xs">-3.26(-0.08%)</p></div></div>'
+    template: '<div class="bg-black border-red-600 dark:bg-gray-800 bg-opacity-95 border-opacity-60 | p-1 border-solid rounded-3xl border-2 | flex justify-around cursor-pointer | hover:bg-red-400 dark:hover:bg-red-600 hover:border-transparent | transition-colors duration-500"><div class="flex flex-col justify-center"><p class="text-base">{{ name }}</p><p class="text-xs">{{ getIndexPrice(name) }}</p><p class="text-xs">-3.26(-0.08%)</p></div></div>',
+    methods:{
+        async getIndexPrice(symbol){
+            let price = await getMarketPrice(symbol);
+            console.log(price);
+            return price;
+    }
+}
 })
 
 App.mount('#App')
@@ -27,7 +27,10 @@ App.mount('#App')
 
 
 async function getMarketPrice(stockSymbol){
-    let url = "https://apidojo-yahoo-finance-v1.p.rapidapi.com/stock/v2/get-summary?symbol="+stockSymbol+"&region=US";
+    let indexMap = new Map([['S&P 500', '%5EGSPC'],['Dow 30', '%5EDJI'],['Nasdaq','%5EIXI']]);
+    let apiStockSymbol = indexMap.get(stockSymbol);
+    let url = "https://apidojo-yahoo-finance-v1.p.rapidapi.com/stock/v2/get-summary?symbol="+apiStockSymbol+"&region=US";
+    
     fetch(url, {
         "method": "GET",
         "headers": {
