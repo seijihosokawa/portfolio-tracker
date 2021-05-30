3dee6776910664fcd00b7eac046de1bbf51a36c2
seijihosokawa <seijihosokawa@gmail.com>
2021-05-30 17:57:30 -0400

    upload csv functionality

--- App.js
+++ App.js
@@ -85,3 +85,16 @@ function getMarketPrice(apiStockSymbol){
         });
     })
 }
+var fileInput = document.getElementById("file-input"),
+
+    readFile = function () {
+        var reader = new FileReader();
+        reader.onload = function () {
+            document.getElementById('out').innerHTML = reader.result;
+            console.log(reader.result)
+        };
+        // start reading the file. When it is done, calls the onload event defined above.
+        reader.readAsBinaryString(fileInput.files[0]);
+    };
+
+fileInput.addEventListener('change', readFile);
